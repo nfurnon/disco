36272e4c5d29c6e0f1a208f474353fa66d62e6dc
HU Mathieu <mathieu.hu@inria.fr>
2020-07-08 09:23:49 +0200

    test for round_to_base

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -8,7 +8,16 @@ import disco_theque.math_utils as dmath
     (102, 10, 100),
     (65, 8, 64),
     (33, 5, 30),
-    )
-)
+    ))
 def test_floor_to_multiple(num, div, expected):
     assert dmath.floor_to_multiple(num, div) == expected
+
+
+@pytest.mark.parametrize('x, base, expected', (
+    (109.56, 5, 110),
+    (108.56, 4, 108),
+    (56, 10, 60),
+    ))
+def test_round_to_base(x, base, expected):
+    assert dmath.round_to_base(x, base=base) == expected
+
