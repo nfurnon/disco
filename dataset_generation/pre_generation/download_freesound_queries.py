685cff6e8f95e7f2728738ca9fb3002d97560736
HU Mathieu <mathieu.hu@inria.fr>
2020-07-10 14:11:15 +0200

    refactor: root logger set up moved to own file

--- dataset_generation/pre_generation/download_freesound_queries.py
+++ dataset_generation/pre_generation/download_freesound_queries.py
@@ -39,6 +39,7 @@ import pandas as pd
 import yaml
 
 import istarmap
+import utils
 
 
 def main(arguments=None):
@@ -49,7 +50,7 @@ def main(arguments=None):
     """
     args = parse_args(arguments)
     config = Config.from_yaml(args.config)
-    logger = set_up_log(level=1)
+    logger = utils.set_up_log(level=1)
 
     if args.num_jobs > 1:
         func_exec = functools.partial(parallel_exec, num_proc=args.num_jobs)
@@ -229,39 +230,6 @@ def extract_category_ids(id_file):
     df = df.iloc[:, 1:]  # skip index column
     df.dropna(inplace=True)
     return df.to_dict(orient='list')
-
-
-def set_up_log(logfile='', level=0):
-    """Sets up root logger.
-
-    Args:
-        logfile (str, optional): Log file. Pass empty string to write to std.err (Default: '')
-        level (int, optional): Verbosity level (Default: 0)
-            * 0: warnings only
-            * 1: info and warnings
-            * otherwise: debug, info and warnings
-
-    Returns:
-        logging.Logger: Formatted root logger
-    """
-    log_format = '[%(levelname)s] %(asctime)s %(funcName)s: %(message)s'
-    time_format = '%Y-%m-%d %H:%M:%S'
-    formatter = logging.Formatter(log_format, time_format)
-    if logfile:
-        os.makedirs(os.path.dirname(logfile), exist_ok=True)
-        handler = logging.FileHandler(logfile)
-    else:
-        handler = logging.StreamHandler(sys.stderr)
-    handler.setFormatter(formatter)
-    logger = logging.getLogger()
-    logger.handlers = [handler]
-    if level == 0:
-        logger.setLevel(logging.WARNING)
-    elif level == 1:
-        logger.setLevel(logging.INFO)
-    else:
-        logger.setLevel(logging.DEBUG)
-    return logger
 
 
 def parallel_exec(func, iterable, num_proc):
