b5aeb348fa954f612b318bdc32f6e85e0f114ab2
HU Mathieu <mathieu.hu@inria.fr>
2020-07-17 13:56:58 +0200

    disco auto-install resources

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+        name='disco',
+        version='0.0.1',
+        author="nfurnon, mhu",
+        packages=find_packages(),
+        python_requires='>=3.6',
+)
